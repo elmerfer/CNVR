2af99c18cb0785ae14be8aed33fc056a35594970
Elmer fernandez <elmerfer@gmail.com>
2022-05-13 16:14:09 -0300

    centromere

--- RunCNVRapp.R
+++ RunCNVRapp.R
@@ -7,7 +7,7 @@ library(plyr)
 .LIBRARY <- "TWIST" #"SSV6" #
 .DB.FULL.NAME <- paste0(.DB.FULL.NAME,.LIBRARY,".RDS")
 
-
+data("Centromere")
 db <- CNVR::LoadDB(.DB.FULL.NAME)
 colnames(db$DB)
 dir.path <- rstudioapi::selectDirectory(path = "/media/respaldo4t/FLENI/CNVs/ONCO/PACIENTES_T" )
